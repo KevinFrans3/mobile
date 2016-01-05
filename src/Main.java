32250af621ac6dcc308b011aeaea8e5727e80aa9
Kevin Frans <kevin@bobthechicken.com>
2016-01-05 14:22:55 -0800

    mains

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,15 @@ public class Main {
 	public static void main(String[] args) {
 		//kek
 		
+		Branch a = new Branch(2,2);
+		Branch b = new Branch(1,2);
+		Mobile ab = new Mobile(a,b);
+		Branch abd = new Branch(2,ab);
+		Branch c = new Branch(4,3);
+		Mobile total = new Mobile(abd,c);
+		
+		
+		System.out.println(total.totalWeight());
 		
 		
 		
