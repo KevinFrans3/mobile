1f7ae58e8871bfafd4c3667c70eb9cd79ede6ad3
Kevin Frans <kevin@bobthechicken.com>
2016-01-05 14:03:54 -0800

    branch

--- src/Branch.java
+++ src/Branch.java
@@ -1,4 +1,13 @@
 
 public class Branch {
+	int length;
+	boolean isWeight = false;
+	int weight;
+	Mobile mobile;
+	
+	public Branch()
+	{
+		
+	}
 
 }
--- src/Mobile.java
+++ src/Mobile.java
@@ -1,4 +1,13 @@
 
 public class Mobile {
+	
+	Branch left;
+	Branch right;
+	
+	public Mobile()
+	{
+		left = new Branch();
+		right = new Branch();
+	}
 
 }
